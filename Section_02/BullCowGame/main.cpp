2d5aa29f20472301a8589d92e7bff674b6256bd1
Arjun  Dass <ad.sam007@gmail.com>
2017-08-25 01:02:14 -0500

    Started BullsAndCow game

--- Section_02/BullCowGame/main.cpp
+++ Section_02/BullCowGame/main.cpp
@@ -1,70 +1,34 @@
-#include <iostream>																		//pre processor directive
+// In order to create a new C++ files, always use Add->New Items
+#include <iostream>			 //we use cascading to include standard stuff
 #include <string>
-#include "FBullCow2.h"
+using namespace std;
 
-using FText = std::string;
-using int32 = int;
-
-void PrintIntro();
-FText GetGuess();
-void PlayGame();
-bool PlayAgain();
-FBullCow BCGame;
-
-void PlayGame();
-
-int32 main() {
-
-	bool bplayAgain = false;
-	do {
-		PrintIntro();
-		PlayGame();
-		bplayAgain = PlayAgain();
-	}while (bplayAgain);
-
-	return 0;
-
-}
-
-//print the intro
 void PrintIntro() {
-													//this thing can be evaluated at the compile time.
-	std::cout << "Welcome to Bull and Cow, a fun word game!!\n";								//std = standard To get into the standard namespace
-	std::cout << "Can you guess the " << BCGame.GetHiddenWordLength();
-	std::cout << " letter isogram I'm thinking of?\n" << std::endl;
+	// Introduction to the game
+	constexpr int word_length = 5;
+
+	std::cout << "Welcome to Bulls and Cow Game! \n";
+	std::cout << "Can you guess the " << word_length << " letter isogram I am thinking of ? " << std::endl;
 	return;
-
 }
 
-void PlayGame()
-{
-
-	BCGame.Reset();
-	int32 MaxTries = BCGame.GetMaxTries;
-
-	for (int32 i = 0; i < MaxTries; i++) {
-		FText Guess = GetGuess();
-
-		FBullCowCount BullCowCount = BCGame.SubmitGuess(Guess);
-		std::cout << "Bulls = " << BullCowCount.Bulls	;
-		std::cout << "Cows = " << BullCowCount.Cows << std::endl;
-	}
-}
-
-//Guess the number
-FText GetGuess() {
-	
-	std::cout << "Enter your guess : ";
-	FText Guess = "";
+std::string GetGuessAndPrintBack() {
+	// Get a guess from the player
+	std::string Guess = ""; // upper case because of Unreal coding standard
+	std::cout << "\nEnter your Guess : ";
 	std::getline(std::cin, Guess);
-	return Guess;
 
+	// Repeat the guess back to them
+	std::cout << "Your guess was " << Guess << std::endl;
+	return Guess;
 }
 
-bool PlayAgain() {
-	std::cout << "Do you wanna play again (y/n)? ";
-	FText Response = "";
-	std::getline(std::cin, Response);
-	return (Response[0] == 'y') || (Response[0] == 'Y');
 
+// The entry point of the application
+int main() {
+
+	PrintIntro();
+	GetGuessAndPrintBack();
+	
+	return 0;
 }
