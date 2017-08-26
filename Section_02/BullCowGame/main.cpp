73f1fb7e49606e7d8bde12aa9d064fbde7cb1fca
Arjun  Dass <ad.sam007@gmail.com>
2017-08-26 01:15:00 -0500

    Added more functionalities

--- Section_02/BullCowGame/main.cpp
+++ Section_02/BullCowGame/main.cpp
@@ -1,6 +1,7 @@
 // In order to create a new C++ files, always use Add->New Items
 #include <iostream>			 //we use cascading to include standard stuff
 #include <string>
+
 using namespace std;
 
 void PrintIntro() {
@@ -12,23 +13,46 @@ void PrintIntro() {
 	return;
 }
 
-std::string GetGuessAndPrintBack() {
+std::string GetGuess() {
+	
 	// Get a guess from the player
 	std::string Guess = ""; // upper case because of Unreal coding standard
 	std::cout << "\nEnter your Guess : ";
 	std::getline(std::cin, Guess);
-
-	// Repeat the guess back to them
-	std::cout << "Your guess was " << Guess << std::endl;
 	return Guess;
+	 
 }
 
+// When we create functions using tools, they are automatically added to the main.h file
+void PlayGame()
+{
+	// Loop for the number of turns asking for guesses
+	constexpr int number_of_turns = 5;
+	for (int i = 0; i < number_of_turns; i++) {
+		std::string Guess = GetGuess();
+
+		// Repeat the guess back to them
+		std::cout << "Your guess was " << Guess << std::endl;
+	}
+}
+
+bool AskToPlayAgain() {
+	std::cout << "\n Do you wanna play again? : ";
+	std::string Response = "";
+	getline(cin, Response);
+	return (Response[0] == 'y') ||  (Response[0] == 'Y');
+}
 
 // The entry point of the application
 int main() {
 
-	PrintIntro();
-	GetGuessAndPrintBack();
+	bool wants_to_play_again = false;
+	do{
+		PrintIntro();
+		PlayGame();
+		wants_to_play_again = AskToPlayAgain();
+	}while (wants_to_play_again);
+	
 	
 	return 0;
 }
