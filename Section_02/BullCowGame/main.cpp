6be9750951926d12ad2fb7840d16957f696ee76e
Arjun  Dass <ad.sam007@gmail.com>
2017-08-29 22:20:12 -0500

    Added everything

--- Section_02/BullCowGame/main.cpp
+++ Section_02/BullCowGame/main.cpp
@@ -1,26 +1,34 @@
+/*
+	This is the console executable, that makes use of the Bullcow
+	This acts as the view in a MVC pattern, and is responsible for all user interaction.
+*/
+
 // In order to create a new C++ files, always use Add->New Items
 #include <iostream>			 //we use cascading to include standard stuff
 #include <string>
 #include "FBullCowGame.h"
 
+using FText = std::string;
+using int32 = int;
+
 FBullCowGame BCGame;	//instantiating an object object of the game
 void PrintIntro() {
 	// Introduction to the game
-	constexpr int word_length = 5;
+	constexpr int32 word_length = 5;
 
 	std::cout << "Welcome to Bulls and Cow Game! \n";
 	std::cout << "Can you guess the " << word_length << " letter isogram I am thinking of ? " << std::endl;
 	return;
 }
 
-std::string GetGuess() {
+FText GetGuess() {
 	
-	int CurrentTry = BCGame.GetCurrentTry();
+	int32 CurrentTry = BCGame.GetCurrentTry();
  
 	std::cout << "Try " << CurrentTry << ". Enter your guess!! : ";
 
 	// Get a guess from the player
-	std::string Guess = ""; // upper case because of Unreal coding standard
+	FText Guess = ""; // upper case because of Unreal coding standard
 	std::cout << "\nEnter your Guess : ";
 	std::getline(std::cin, Guess);
 	return Guess;
@@ -31,14 +39,14 @@ std::string GetGuess() {
 void PlayGame()
 {
 	BCGame.Reset();
-	int MaxTries = BCGame.GetMaxTries();
+	int32 MaxTries = BCGame.GetMaxTries();
 
 	// Loop for the number of turns asking for guesses
-	constexpr int number_of_turns = 5;
+	constexpr int32 number_of_turns = 5;
 
-	//TODO Chnge from for to a while loop
-	for (int i = 0; i < number_of_turns; i++) {
-		std::string Guess = GetGuess();	// TODO make loop checking valid
+	//TODO Chnge from for to a while loop 
+	for (int32 i = 0; i < number_of_turns; i++) {
+		FText Guess = GetGuess();	// TODO make loop checking valid
 
 		// Submit valid guess to the game	
 		std::cout << "Your guess was " << Guess << std::endl;
@@ -47,13 +55,13 @@ void PlayGame()
 
 bool AskToPlayAgain() {
 	std::cout << "\n Do you wanna play again? : ";
-	std::string Response = "";
+	FText Response = "";
 	getline(std::cin, Response);
 	return (Response[0] == 'y') ||  (Response[0] == 'Y');
 }
 
 // The entry point of the application
-int main() {
+int32 main() {
 
 	bool wants_to_play_again = false;
 	do{
