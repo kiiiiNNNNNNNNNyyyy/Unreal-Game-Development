304556da4de16795d87a89476a20c1770a351fa7
Arjun  Dass <ad.sam007@gmail.com>
2017-08-30 09:56:54 -0500

    Fixed the bug, and added a new function

--- Section_02/BullCowGame/main.cpp
+++ Section_02/BullCowGame/main.cpp
@@ -1,81 +1,85 @@
-/*
-	This is the console executable, that makes use of the Bullcow
-	This acts as the view in a MVC pattern, and is responsible for all user interaction.
+/* This is the console executable, that makes use of the BullCow class
+This acts as the view in a MVC pattern, and is responsible for all
+user interaction. For game logic see the FBullCowGame class.
 */
 
-// In order to create a new C++ files, always use Add->New Items
-#include <iostream>			 //we use cascading to include standard stuff
-#include <string>
+#include <iostream>
+#include <string>	
 #include "FBullCowGame.h"
 
 using FText = std::string;
 using int32 = int;
 
-FBullCowGame BCGame;	//instantiating an object object of the game
-void PrintIntro() {
-	// Introduction to the game
-	constexpr int32 word_length = 5;
+void PrintIntro();
+void PlayGame();
+FText GetGuess();
+bool AskToPlayAgain();
 
-	std::cout << "Welcome to Bulls and Cow Game! \n";
-	std::cout << "Can you guess the " << word_length << " letter isogram I am thinking of ? " << std::endl;
+FBullCowGame BCGame; // instantiate a new game
+
+					 // the entry point for our application
+int main()
+{
+	bool bPlayAgain = false;
+	do {
+		PrintIntro();
+		PlayGame();
+		bPlayAgain = AskToPlayAgain();
+	} while (bPlayAgain);
+
+	return 0; // exit the application
+}
+
+
+// introduce the game
+void PrintIntro()
+{
+	std::cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	std::cout << "Can you guess the " << BCGame.GetHiddenWordLength();
+	std::cout << " letter isogram I'm thinking of?\n";
+	std::cout << std::endl;
 	return;
 }
 
-FText GetGuess() {
-	
-	int32 CurrentTry = BCGame.GetCurrentTry();
- 
-	std::cout << "Try " << CurrentTry << ". Enter your guess!! : ";
 
-	// Get a guess from the player
-	FText Guess = ""; // upper case because of Unreal coding standard
-	std::cout << "\nEnter your Guess : ";
-	std::getline(std::cin, Guess);
-	return Guess;
-	 
-}
-
-// When we create functions using tools, they are automatically added to the main.h file
 void PlayGame()
 {
 	BCGame.Reset();
 	int32 MaxTries = BCGame.GetMaxTries();
 
-	// Loop for the number of turns asking for guesses
-	constexpr int32 number_of_turns = 5;
+	// loop for the number of turns asking for guesses
+	// TODO change from FOR to WHILE loop once we are validating tries
+	for (int32 count = 1; count <= MaxTries; count++) {
+		FText Guess = GetGuess(); // TODO make loop checking valid
 
-	//TODO Chnge from for to a while loop 
-	for (int32 i = 0; i < number_of_turns; i++) {
-		FText Guess = GetGuess();	// TODO make loop checking valid
-
-		// Submit valid guess to the game
+								  // submit valid guess to the game, and receive counts
 		FBullCowCount BullCowCount = BCGame.SubmitGuess(Guess);
+		// print number of bulls and cows
 		std::cout << "Bulls = " << BullCowCount.Bulls;
-		std::cout << " .Cows = " << BullCowCount.Cows << std::endl;
+		std::cout << ". Cows = " << BullCowCount.Cows << std::endl;
 
-		std::cout << "Your guess was " << Guess << std::endl;
+		std::cout << std::endl;
 	}
+
+	// TODO summarise game
 }
 
-bool AskToPlayAgain() {
-	std::cout << "\n Do you wanna play again? : ";
+
+FText GetGuess()
+{
+	int32 CurrentTry = BCGame.GetCurrentTry();
+
+	// get a guess from the player
+	std::cout << "Try " << CurrentTry << ". Enter your guess: ";
+	FText Guess = "";
+	std::getline(std::cin, Guess);
+	return Guess;
+}
+
+bool AskToPlayAgain()
+{
+	std::cout << "Do you want to play again (y/n)? ";
 	FText Response = "";
-	getline(std::cin, Response);
-	return (Response[0] == 'y') ||  (Response[0] == 'Y');
-}
-
-// The entry point of the application
-int32 main() {
-
-	bool wants_to_play_again = false;
-	do{
-		PrintIntro();
-		PlayGame();
-
-		// TODO -  Add todo list to added to the task list in visual studio task list
-		wants_to_play_again = AskToPlayAgain();
-	}while (wants_to_play_again);
-	
-	
-	return 0;
+	std::getline(std::cin, Response);
+	return (Response[0] == 'y') || (Response[0] == 'Y');
 }
